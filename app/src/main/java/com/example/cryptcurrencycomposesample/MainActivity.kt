b185f72d6dec2ed41450e48afb92a0f02528c8d9
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2021-12-18 23:53:43 +0900

    Update MainActivity.kt

--- app/src/main/java/com/example/cryptcurrencycomposesample/MainActivity.kt
+++ app/src/main/java/com/example/cryptcurrencycomposesample/MainActivity.kt
@@ -18,16 +18,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CryptCurrencyComposeSampleTheme {
-        Greeting("Android")
-    }
-}

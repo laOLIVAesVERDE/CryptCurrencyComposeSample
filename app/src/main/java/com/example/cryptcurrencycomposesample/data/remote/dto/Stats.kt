29aac4a8463717bac38581af92a7d42e1f4a76b0
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2021-12-19 23:30:21 +0900

    update

---
+++ app/src/main/java/com/example/cryptcurrencycomposesample/data/remote/dto/Stats.kt
@@ -0,0 +1,13 @@
+package com.example.cryptcurrencycomposesample.data.remote.dto
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Stats(
+    @SerializedName("contributors")
+    val contributors: Int,
+    @SerializedName("stars")
+    val stars: Int,
+    @SerializedName("subscribers")
+    val subscribers: Int
+)

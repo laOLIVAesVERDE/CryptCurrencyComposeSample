f39e0a06db30ee57de9450f1f1bf8eeec647bddd
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2021-12-19 23:29:02 +0900

    update

--- app/src/main/java/com/example/cryptcurrencycomposesample/data/remote/CoinPaprikaApi.kt
+++ app/src/main/java/com/example/cryptcurrencycomposesample/data/remote/CoinPaprikaApi.kt
@@ -1,8 +1,12 @@
 package com.example.cryptcurrencycomposesample.data.remote
 
+import com.example.cryptcurrencycomposesample.data.remote.dto.CoinDto
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface CoinPaprikaApi {
     @GET("/v1/coins")
-    suspend fun getCoins()
+    suspend fun getCoins(): List<CoinDto>
+    @GET("/v1/coins/{coinId}")
+    suspend fun getCoinById(@Path("coinId") coinId: String)
 }

32f7e326b6bcbc4fd3fa1bf30607e5af5a63db61
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2021-12-22 22:27:04 +0900

    Update GetCoinsUseCase.kt

--- app/src/main/java/com/example/cryptcurrencycomposesample/domain/use_case/get_coins/GetCoinsUseCase.kt
+++ app/src/main/java/com/example/cryptcurrencycomposesample/domain/use_case/get_coins/GetCoinsUseCase.kt
@@ -1,11 +1,13 @@
 package com.example.cryptcurrencycomposesample.domain.use_case.get_coins
 
 import com.example.cryptcurrencycomposesample.common.Resource
+import com.example.cryptcurrencycomposesample.data.remote.dto.toCoin
 import com.example.cryptcurrencycomposesample.domain.model.Coin
 import com.example.cryptcurrencycomposesample.domain.repository.CoinRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import retrofit2.HttpException
+import java.io.IOException
 import javax.inject.Inject
 
 class GetCoinsUseCase @Inject constructor(
@@ -13,8 +15,11 @@ class GetCoinsUseCase @Inject constructor(
 ) {
     operator fun invoke(): Flow<Resource<List<Coin>>> = flow {
         try {
-
+            emit(Resource.Loading())
+            val coins = coinRepository.getCoins().map { it.toCoin() }
         } catch (e: HttpException) {
+
+        } catch (e: IOException) {
 
         }
     }

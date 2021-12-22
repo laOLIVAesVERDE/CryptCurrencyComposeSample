4114630d7cc993a791113f7a91a6ce4632494bea
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2021-12-22 22:14:40 +0900

    Update CoinRepositoryImpl.kt

--- app/src/main/java/com/example/cryptcurrencycomposesample/data/repository/CoinRepositoryImpl.kt
+++ app/src/main/java/com/example/cryptcurrencycomposesample/data/repository/CoinRepositoryImpl.kt
@@ -1,8 +1,9 @@
 package com.example.cryptcurrencycomposesample.data.repository
 
+import com.example.cryptcurrencycomposesample.data.remote.CoinPaprikaApi
 import javax.inject.Inject
 
 class CoinRepositoryImpl @Inject constructor(
-    
+    private val api: CoinPaprikaApi
 ) {
 }

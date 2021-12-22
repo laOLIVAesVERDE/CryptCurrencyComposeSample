6bf8bfdce388db4a6439bc7b7cb75666d266508f
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2021-12-22 22:15:10 +0900

    Update CoinRepositoryImpl.kt

--- app/src/main/java/com/example/cryptcurrencycomposesample/data/repository/CoinRepositoryImpl.kt
+++ app/src/main/java/com/example/cryptcurrencycomposesample/data/repository/CoinRepositoryImpl.kt
@@ -1,9 +1,19 @@
 package com.example.cryptcurrencycomposesample.data.repository
 
 import com.example.cryptcurrencycomposesample.data.remote.CoinPaprikaApi
+import com.example.cryptcurrencycomposesample.data.remote.dto.CoinDetailDto
+import com.example.cryptcurrencycomposesample.data.remote.dto.CoinDto
+import com.example.cryptcurrencycomposesample.domain.repository.CoinRepository
 import javax.inject.Inject
 
 class CoinRepositoryImpl @Inject constructor(
     private val api: CoinPaprikaApi
-) {
+) : CoinRepository {
+    override suspend fun getCoins(): List<CoinDto> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getCoinById(coinId: String): CoinDetailDto {
+        TODO("Not yet implemented")
+    }
 }

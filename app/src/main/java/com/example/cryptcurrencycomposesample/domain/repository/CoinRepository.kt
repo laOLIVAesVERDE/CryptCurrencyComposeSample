ab8b4ec9779ff6f347c92f51ac14aa268044af98
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2021-12-22 22:11:08 +0900

    Update CoinRepository.kt

--- app/src/main/java/com/example/cryptcurrencycomposesample/domain/repository/CoinRepository.kt
+++ app/src/main/java/com/example/cryptcurrencycomposesample/domain/repository/CoinRepository.kt
@@ -1,8 +1,9 @@
 package com.example.cryptcurrencycomposesample.domain.repository
 
+import com.example.cryptcurrencycomposesample.data.remote.dto.CoinDetailDto
 import com.example.cryptcurrencycomposesample.data.remote.dto.CoinDto
 
 interface CoinRepository {
     suspend fun getCoins(): List<CoinDto>
-
+    suspend fun getCoinById(coinId: String): CoinDetailDto
 }

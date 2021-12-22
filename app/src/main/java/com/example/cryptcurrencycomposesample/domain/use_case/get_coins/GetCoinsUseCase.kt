a94aff0623d9885a04dea290287d960d8dfa2c37
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2021-12-22 22:22:05 +0900

    Update GetCoinsUseCase.kt

--- app/src/main/java/com/example/cryptcurrencycomposesample/domain/use_case/get_coins/GetCoinsUseCase.kt
+++ app/src/main/java/com/example/cryptcurrencycomposesample/domain/use_case/get_coins/GetCoinsUseCase.kt
@@ -1,9 +1,15 @@
 package com.example.cryptcurrencycomposesample.domain.use_case.get_coins
 
+import com.example.cryptcurrencycomposesample.common.Resource
+import com.example.cryptcurrencycomposesample.domain.model.Coin
 import com.example.cryptcurrencycomposesample.domain.repository.CoinRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetCoinsUseCase @Inject constructor(
     private val coinRepository: CoinRepository
 ) {
+    operator fun invoke(): Flow<Resource<List<Coin>>> {
+        
+    }
 }

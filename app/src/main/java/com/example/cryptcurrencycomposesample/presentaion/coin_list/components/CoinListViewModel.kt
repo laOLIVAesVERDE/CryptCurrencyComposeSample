b72a968d3403a53b2a907a2b7e45016b84acf376
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2021-12-24 23:43:37 +0900

    Update CoinListViewModel.kt

--- app/src/main/java/com/example/cryptcurrencycomposesample/presentaion/coin_list/components/CoinListViewModel.kt
+++ app/src/main/java/com/example/cryptcurrencycomposesample/presentaion/coin_list/components/CoinListViewModel.kt
@@ -3,9 +3,11 @@ package com.example.cryptcurrencycomposesample.presentaion.coin_list.components
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.example.cryptcurrencycomposesample.common.Resource
 import com.example.cryptcurrencycomposesample.domain.use_case.get_coins.GetCoinsUseCase
 import com.example.cryptcurrencycomposesample.presentaion.coin_list.CoinListState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 @HiltViewModel
@@ -14,4 +16,20 @@ class CoinListViewModel @Inject constructor(
 ) : ViewModel() {
   private val _state = mutableStateOf(CoinListState())
   val state: State<CoinListState> = _state
+
+  private fun getCoins() {
+    getCoinsUseCase().onEach { result ->
+      when (result) {
+        is Resource.Success -> {
+
+        }
+        is Resource.Error -> {
+
+        }
+        is Resource.Loading -> {
+          
+        }
+      }
+    }
+  }
 }

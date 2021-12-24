e0f369e68c37368bde993da66520b66a8dd809f9
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2021-12-24 23:11:02 +0900

    Update AppModule.kt

--- app/src/main/java/com/example/cryptcurrencycomposesample/di/AppModule.kt
+++ app/src/main/java/com/example/cryptcurrencycomposesample/di/AppModule.kt
@@ -1,10 +1,18 @@
 package com.example.cryptcurrencycomposesample.di
 
+import com.example.cryptcurrencycomposesample.data.remote.CoinPaprikaApi
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+    @Provides
+    @Singleton
+    fun providePaprikaApi(): CoinPaprikaApi {
+        
+    }
 }

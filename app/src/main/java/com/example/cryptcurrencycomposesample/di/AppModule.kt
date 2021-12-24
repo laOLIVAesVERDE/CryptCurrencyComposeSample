5b2ea39a2033616d957ee551fd2864fb63954ac2
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2021-12-24 23:14:11 +0900

    Update AppModule.kt

--- app/src/main/java/com/example/cryptcurrencycomposesample/di/AppModule.kt
+++ app/src/main/java/com/example/cryptcurrencycomposesample/di/AppModule.kt
@@ -1,10 +1,14 @@
 package com.example.cryptcurrencycomposesample.di
 
+import com.example.cryptcurrencycomposesample.common.Constants
 import com.example.cryptcurrencycomposesample.data.remote.CoinPaprikaApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 import javax.inject.Singleton
 
 @Module
@@ -13,6 +17,10 @@ object AppModule {
     @Provides
     @Singleton
     fun providePaprikaApi(): CoinPaprikaApi {
-        
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CoinPaprikaApi::class.java)
     }
 }

a846256bfddde71c838500c35fef4b316284d416
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2021-12-27 00:02:50 +0900

    Update CoinListScreen.kt

--- app/src/main/java/com/example/cryptcurrencycomposesample/presentaion/coin_list/components/CoinListScreen.kt
+++ app/src/main/java/com/example/cryptcurrencycomposesample/presentaion/coin_list/components/CoinListScreen.kt
@@ -1,6 +1,11 @@
 package com.example.cryptcurrencycomposesample.presentaion.coin_list.components
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 
@@ -9,5 +14,17 @@ fun CoinListScreen(
     navController: NavController,
     viewModel: CoinListViewModel = hiltViewModel()
 ) {
-    
+    val state = viewModel.state.value
+    Box(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(modifier = Modifier.fillMaxSize(), content = {
+            items(state.coins) { coin ->
+                CoinListItem(
+                    coin = coin,
+                    onItemClick = {
+                        
+                    }
+                )
+            }
+        })
+    }
 }

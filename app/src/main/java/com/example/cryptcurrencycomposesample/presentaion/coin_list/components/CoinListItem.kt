6403277132ab94f2b90c12f6ab8cb0572fe60984
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2021-12-26 23:35:41 +0900

    Update CoinListItem.kt

--- app/src/main/java/com/example/cryptcurrencycomposesample/presentaion/coin_list/components/CoinListItem.kt
+++ app/src/main/java/com/example/cryptcurrencycomposesample/presentaion/coin_list/components/CoinListItem.kt
@@ -1,8 +1,13 @@
 package com.example.cryptcurrencycomposesample.presentaion.coin_list.components
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.example.cryptcurrencycomposesample.domain.model.Coin
 
 @Composable
-fun CoinListItem() {
+fun CoinListItem(
+    coin: Coin,
+    onItemClick: (Coin) -> Unit
+) {
     
 }

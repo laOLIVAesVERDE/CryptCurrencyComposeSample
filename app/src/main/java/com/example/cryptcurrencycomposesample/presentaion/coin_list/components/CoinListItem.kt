1fd2e820e02994a062d2e7f5accebc22373ec303
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2021-12-26 23:38:57 +0900

    Update CoinListItem.kt

--- app/src/main/java/com/example/cryptcurrencycomposesample/presentaion/coin_list/components/CoinListItem.kt
+++ app/src/main/java/com/example/cryptcurrencycomposesample/presentaion/coin_list/components/CoinListItem.kt
@@ -1,13 +1,28 @@
 package com.example.cryptcurrencycomposesample.presentaion.coin_list.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.cryptcurrencycomposesample.domain.model.Coin
+
 
 @Composable
 fun CoinListItem(
     coin: Coin,
     onItemClick: (Coin) -> Unit
 ) {
-    
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onItemClick(coin) }
+            .padding(20.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+    }
 }

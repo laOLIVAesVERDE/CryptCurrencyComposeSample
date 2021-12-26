8d710591b8ba904b3acc68d88f594337b4ad9e6f
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2021-12-26 23:50:22 +0900

    Update CoinListScreen.kt

--- app/src/main/java/com/example/cryptcurrencycomposesample/presentaion/coin_list/components/CoinListScreen.kt
+++ app/src/main/java/com/example/cryptcurrencycomposesample/presentaion/coin_list/components/CoinListScreen.kt
@@ -1,8 +1,13 @@
 package com.example.cryptcurrencycomposesample.presentaion.coin_list.components
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
 
 @Composable
-fun CoinListScreen() {
+fun CoinListScreen(
+    navController: NavController,
+    viewModel: CoinListViewModel = hiltViewModel()
+) {
     
 }

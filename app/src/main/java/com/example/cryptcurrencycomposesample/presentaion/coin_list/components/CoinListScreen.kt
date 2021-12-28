9db95883b17ba5173865be736eb1d1868497f720
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2021-12-28 19:37:16 +0900

    Update CoinListScreen.kt

--- app/src/main/java/com/example/cryptcurrencycomposesample/presentaion/coin_list/components/CoinListScreen.kt
+++ app/src/main/java/com/example/cryptcurrencycomposesample/presentaion/coin_list/components/CoinListScreen.kt
@@ -2,10 +2,16 @@ package com.example.cryptcurrencycomposesample.presentaion.coin_list.components
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.cryptcurrencycomposesample.presentaion.Screen
@@ -28,6 +34,17 @@ fun CoinListScreen(
                     }
                 )
             }
+        }
+        if (state.error.isNotBlank()) {
+            Text(
+                text = state.error,
+                color = MaterialTheme.colors.error,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 20.dp)
+                    .align(Alignment.Center)
+            )
         }
     }
 }

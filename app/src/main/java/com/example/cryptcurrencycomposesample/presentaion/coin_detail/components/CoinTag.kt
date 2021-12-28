ea0104ff995966e2541c268681e6373372a3b60b
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2021-12-28 19:52:14 +0900

    Update CoinTag.kt

--- app/src/main/java/com/example/cryptcurrencycomposesample/presentaion/coin_detail/components/CoinTag.kt
+++ app/src/main/java/com/example/cryptcurrencycomposesample/presentaion/coin_detail/components/CoinTag.kt
@@ -1,8 +1,23 @@
 package com.example.cryptcurrencycomposesample.presentaion.coin_detail.components
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun CoinTag() {
-    
+fun CoinTag(tag: String) {
+    Box(
+        modifier = Modifier
+            .border(
+                width = 1.dp,
+                color = MaterialTheme.colors.primary,
+                shape = RoundedCornerShape(100.dp)
+            )
+    ) {
+
+    }
 }

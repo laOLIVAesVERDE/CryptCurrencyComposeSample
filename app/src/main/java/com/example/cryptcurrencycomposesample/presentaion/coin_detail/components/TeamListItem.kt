4adb4e3a1ef0f0803bc778552b2e27482d67e98a
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2021-12-28 19:58:06 +0900

    Create TeamListItem.kt

---
+++ app/src/main/java/com/example/cryptcurrencycomposesample/presentaion/coin_detail/components/TeamListItem.kt
@@ -0,0 +1,13 @@
+package com.example.cryptcurrencycomposesample.presentaion.coin_detail.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.cryptcurrencycomposesample.data.remote.dto.TeamMember
+
+@Composable
+fun TeamListItem(
+    teamMember: TeamMember,
+    modifier: Modifier = Modifier
+) {
+
+}

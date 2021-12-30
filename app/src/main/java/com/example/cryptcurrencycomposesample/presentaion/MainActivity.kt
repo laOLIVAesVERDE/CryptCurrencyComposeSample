e61663688ef24cea627ff00e39aea91e903aefca
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2021-12-30 18:20:48 +0900

    Update MainActivity.kt

--- app/src/main/java/com/example/cryptcurrencycomposesample/presentaion/MainActivity.kt
+++ app/src/main/java/com/example/cryptcurrencycomposesample/presentaion/MainActivity.kt
@@ -3,12 +3,26 @@ package com.example.cryptcurrencycomposesample.presentaion
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.example.cryptcurrencycomposesample.presentaion.ui.theme.CryptocurrencyAppYTTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            CryptocurrencyAppYTTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.CoinListScreen.route) {
+                        
+                    }
+                }
+            }
         }
     }
 }
